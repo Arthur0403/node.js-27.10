15e1e3a363847cda0d0fe7b46477d27817cad56d
Dmitry <lukasm@yandex.ru>
2018-11-02 18:48:36 +0300

    Поправил вывод данных

--- Lukyanov_Dmitry/HW-2/game.js
+++ Lukyanov_Dmitry/HW-2/game.js
@@ -3,12 +3,9 @@ const fs = require('fs');
 const random = require('random');
 const chalk = require('chalk');
 
-
-
 let games = 1;
 let trueAnswer = 0;
 let falseAnswer = 0;
-
 
 fs.writeFileSync('./log.txt', `\nИгра ${games}` );
 const rl = readline.createInterface({
@@ -26,16 +23,13 @@ rl.on('line', (inputDigit) => {
 
 
   if (inputDigit > 2 || inputDigit < 1) {
-    //console.log('Нужно выбрать число 1 или 2');
-
-    //console.log('Нужно выбрать число 1 или 2');
-    console.log(chalk.white.bgRed(' Нужно выбрать число 1 или 2 \n'));
+    console.log(chalk.white.bgRed(' Нужно выбрать число 1 или 2 '));
     
   } else if (isNaN(inputDigit)) {
-    console.log('Вы ввели некоректный символ!');
-    return;
+    console.log(chalk.white.bgRed(' Ты выбрал не число! Нужно выбрать число 1 или 2 '));
+    
   } else if (inputDigit == rnd) {
-    console.log(chalk.white.bgGreen(' Правильно '));
+    console.log(chalk.white.bgGreen(' Угадал! Молодец '));
     trueAnswer++;
     
   } else if (inputDigit !== rnd) {
@@ -44,39 +38,10 @@ rl.on('line', (inputDigit) => {
   }
   
   
-  fs.writeFileSync('./log.txt', `Всего попыток - ${games} \n--Угадал: ${trueAnswer} раз. \n--Неугадал: ${falseAnswer} раз. \n--ИТОГ = ${trueAnswer}:${falseAnswer} \n _____________________________`  );
+  fs.writeFileSync('./log.txt', chalk.yellow(`--------------------\nВсего попыток - ${games} \n--Угадал: ${trueAnswer} раз. \n--Неугадал: ${falseAnswer} раз. \n--ИТОГ-- ${trueAnswer}:${falseAnswer} \n--------------------`  ));
   var fileContent = fs.readFileSync("log.txt", "utf8");
   console.log(fileContent);
   games++;
-  console.log('\nВыбери число '+ (chalk.black.bgYellow (' 1 ')) + ' или ' + (chalk.black.bgYellow (' 2 ')));
+  console.log('\nПродолжим \nВыбери число '+ (chalk.black.bgYellow (' 1 ')) + ' или ' + (chalk.black.bgYellow (' 2 ')));
 });
 
-
-
-
-
-
-
-
-
-// const stopPlay = (games) => {
-//   console.log(chalk.green('Правильно'));
-//   rl.question('Продолжим? Да или Нет\n', (answer) => {
-//     if (answer == 'нет') {
-//       console.log('\n Спасибо за игру!');
-//       rl.close();
-      
-//     } else {
-//       console.log('\nВыбери число 1 или 2');
-//     }
-//   });
-
-//  let nunber = 1;
-//  const resNumber = (number) => {
-//   number++;
-//  };
-
-  
-  //return 0;
-//};
- 
