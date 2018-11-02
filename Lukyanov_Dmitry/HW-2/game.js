08c7f480f7abefd986bb378770673a64cc64626a
Dmitry <lukasm@yandex.ru>
2018-11-02 21:19:12 +0300

    Исправал статистику

--- Lukyanov_Dmitry/HW-2/game.js
+++ Lukyanov_Dmitry/HW-2/game.js
@@ -13,7 +13,7 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-console.log('\nВыбери число '+ (chalk.black.bgYellow (' 1 ')) + ' или ' + (chalk.black.bgYellow (' 2 ')));
+console.log('\nВыбери число '+ (chalk.black.bgYellow(' 1 ')) + ' или ' + (chalk.black.bgYellow.bold(' 2 ')));
 
 
 rl.on('line', (inputDigit) => {
@@ -24,13 +24,13 @@ rl.on('line', (inputDigit) => {
 
   if (inputDigit > 2) {
     console.log(chalk.white.bgRed(' Ты выбрал число больше 2 ! Нужно выбрать число 1 или 2 '));
-
+    games--;
   } else if (inputDigit < 1) {
     console.log(chalk.white.bgRed(' Ты выбрал число меньше 1 ! Нужно выбрать число 1 или 2 '));
-    
+    games--;
   } else if (isNaN(inputDigit)) {
     console.log(chalk.white.bgRed(' Ты выбрал не число! Нужно выбрать число 1 или 2 '));
-    
+    games--;
   } else if (inputDigit == rnd) {
     console.log(chalk.white.bgGreen(' Угадал! Молодец '));
     trueAnswer++;
