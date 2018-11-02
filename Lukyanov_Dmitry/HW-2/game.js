4bf552cbf6119ec56432a31ba0f7ca643ccb5d98
Dmitry <lukasm@yandex.ru>
2018-11-02 21:22:09 +0300

    edit

--- Lukyanov_Dmitry/HW-2/game.js
+++ Lukyanov_Dmitry/HW-2/game.js
@@ -7,20 +7,15 @@ let games = 1;
 let trueAnswer = 0;
 let falseAnswer = 0;
 
-fs.writeFileSync('./log.txt', `\nИгра ${games}`,"utf8" );
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+fs.writeFileSync('./log.txt', `\nИгра ${games}`, "utf8");
+const rl = readline.createInterface({input: process.stdin, output: process.stdout})
 
-console.log('\nВыбери число '+ (chalk.black.bgYellow(' 1 ')) + ' или ' + (chalk.black.bgYellow.bold(' 2 ')));
-
+console.log('\nВыбери число ' + (chalk.black.bgYellow(' 1 ')) + ' или ' + (chalk.black.bgYellow.bold(' 2 ')));
 
 rl.on('line', (inputDigit) => {
   const rnd = random.int(1, 2);
   console.log('Ты выбрал:', inputDigit);
-  console.log ('Компьютер загадал:' ,rnd);
-
+  console.log('Компьютер загадал:', rnd);
 
   if (inputDigit > 2) {
     console.log(chalk.white.bgRed(' Ты выбрал число больше 2 ! Нужно выбрать число 1 или 2 '));
@@ -34,17 +29,15 @@ rl.on('line', (inputDigit) => {
   } else if (inputDigit == rnd) {
     console.log(chalk.white.bgGreen(' Угадал! Молодец '));
     trueAnswer++;
-    
+
   } else if (inputDigit !== rnd) {
     console.log(chalk.white.bgRed(' Не угадал! Попробуй еще раз '));
     falseAnswer++;
   }
-  
-  
+
   fs.writeFileSync('./log.txt', chalk.yellow(`--------------------\nВсего попыток - ${games} \n--Угадал: ${trueAnswer} раз. \n--Неугадал: ${falseAnswer} раз. \n--ИТОГ-- ${trueAnswer}:${falseAnswer} \n--------------------`));
   var fileContent = fs.readFileSync("log.txt", "utf8");
   console.log(fileContent);
   games++;
-  console.log('\nПродолжим \nВыбери число '+ (chalk.black.bgYellow (' 1 ')) + ' или ' + (chalk.black.bgYellow (' 2 ')));
+  console.log('\nПродолжим \nВыбери число ' + (chalk.black.bgYellow(' 1 ')) + ' или ' + (chalk.black.bgYellow(' 2 ')));
 });
-
