ff9c3ac26dcb42e488b1fe6abdfe204dec9098ba
Aibek <tokhtokhodzhaev@gmail.com>
2018-11-03 13:58:36 +0600

    правочки

--- Aibek/lesson2/index.js
+++ Aibek/lesson2/index.js
@@ -5,32 +5,51 @@
 				output:process.stdout
 			})
 
-			var a
-			var b
 			var wins=0
 			var losses=0
 			var number=0
-			while (true){
-				rl.question('Введите число от 1 до 2',(answer)=>
+			async function getNum (){
+				return new Promise((resolve,reject) => {
+					rl.question('Введите число',(answer)=>
 					{
-						b = answer
+						var b = answer;
+						console.log(b);
+						resolve(b);
 					});
-				console.log(b)
-				a = Math.floor(Math.random()*2)+1;
-				console.log(a);
-				if (a==b){
-					console.log('Match!')
-					wins++
-				}
-				else if (b==3){
-					break
-				}
-				else {
-					console.log('Unlucky :(')
-					losses++
-				}
+					
+				})
 			}
-			number++
-			console.log(wins)
-			console.log(losses)
-			console.log(number)
+			async function commonDoIt(){
+				return new Promise(async(resolve,reject) =>{
+					var b = await getNum();
+					var a = Math.floor(Math.random()*2)+1;
+					console.log(a);
+					if (a==b){
+						console.log('Match!')
+						wins++
+					}
+					else if(b==3){
+						resolve (false);
+					}
+					else {
+						console.log('Unlucky :(')
+						losses++
+					}
+					resolve (true)
+					})
+				
+			}
+			async function start(){
+				for (var x=0;x<=5;x++){
+
+					var c =await commonDoIt()
+					console.log(c)
+					
+					number++
+					console.log(wins)
+					console.log(losses)
+					console.log(number)
+				}
+				return;
+			}
+			start();
