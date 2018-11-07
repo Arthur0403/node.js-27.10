45a10836f604578892464257eac63ce6b2b0df82
VadimBond <wadimbond@mail.ru>
2018-11-07 22:46:53 +0100

    lesson3 VadimBond (error correction)

--- VadimBond/tran.js
+++ VadimBond/tran.js
@@ -5,22 +5,40 @@
  */
 
 const request = require('request');
-
-let word = 'boy';
-console.log('Переводим слово: ', word);
+const readline = require('readline');
 
 const myKey = 'trnsl.1.1.20181107T120934Z.c64b5a4b7d56ba8d.531d97fac16dff15098b4182d2fc696486a50c82';
-let reqStart = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=';
-let reqEnd = '&lang=en-ru&text=';
-let uri = reqStart + myKey + reqEnd + word;
+const reqStart = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=';
+const reqEnd = '&lang=en-ru&text=';
 
-request({
-	uri: uri,
-	followAllRedirects: true,
-	method: 'GET',
-	json: true
-}, (err, response, body) => {
-	if (!err && response.statusCode === 200) {
-		console.log('Перевод на русский язык: ', body.text[0]);
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.on('line', (input) => {
+	if(input === 'q') {
+		rl.close();
+	} else if(!input) {
+			console.log(`Введите слово для перевода или 'q' для выхода`);
+	} else {
+			translator(input);
 	}
 });
+
+rl.emit('line', '');
+
+const translator = (word) => {
+	const uri = reqStart + myKey + reqEnd + word;
+	return request({
+		uri: uri,
+		followAllRedirects: true,
+		method: 'GET',
+		json: true
+	}, (err, response, body) => {
+		if (!err && response.statusCode === 200) {
+			console.log(`Перевод на русский язык: ${body.text[0]}`);
+		}
+	});
+};
+
