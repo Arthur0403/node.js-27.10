4c328562a76b597d5caf77b9f9d6b365b904dc86
Dmitry <lukasm@yandex.ru>
2018-11-07 23:21:05 +0300

    Правка

--- Lukyanov_Dmitry/HW-3/parsing.js
+++ Lukyanov_Dmitry/HW-3/parsing.js
@@ -2,23 +2,26 @@ const request = require('request');
 const cheerio = require('cheerio');
 const chalk = require('chalk');
 
+//Запрос
 request('https://news.yandex.ru/', (err, response, body) => {
   if (!err && response.statusCode === 200) {
-   const $ = cheerio.load(body);
-   
-   // Количество новостей
-   const number = $('.story__title').contents().length;
-   console.log (chalk.white.bgRed(` Всего новостей - ${number}  `));
-   
-   //Вывод новостей
-   const news = [];
-     $('.story__title').each(function(i) {
-     news[i] = (i+1) + '. ' + $(this).text();
-   });
-   const allNews = news.join('\n');
-   console.log(`${allNews}`)
-  
+    const $ = cheerio.load(body);
+
+    // Количество новостей
+    const number = $('.story__title')
+      .contents()
+      .length;
+    console.log(chalk.white.bgRed(` Всего новостей - ${number}  `));
+
+    //Вывод новостей
+    const news = [];
+    $('.story__title').each(function (i) {
+      news[i] = (i + 1) + '. ' + $(this).text();
+    });
+    const allNews = news.join('\n');
+    console.log(`${allNews}`)
+
   } else {
-    console.log (chalk.white.bgRed(' Нет ответа от сервера! '));
+    console.log(chalk.white.bgRed(' Нет ответа от сервера! '));
   }
 });
