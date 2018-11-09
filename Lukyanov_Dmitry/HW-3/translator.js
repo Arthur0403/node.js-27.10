4072f26bf28f4a37b6c54804bf3fb365a3da2d83
Dmitry <lukasm@yandex.ru>
2018-11-10 00:16:54 +0300

    Добавил HW-4

--- Lukyanov_Dmitry/HW-3/translator.js
+++ Lukyanov_Dmitry/HW-3/translator.js
@@ -1,29 +1,117 @@
-const request = require('request');
-const readline = require('readline');
-const chalk = require('chalk');
+const request = require("request");
+const cheerio = require("cheerio");
+const express = require("express");
+const temph = require("consolidate");
+const bodyParser = require("body-parser");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const app = express();
+app.engine('hbs', temph.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', '');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res){
+    //написал союственную функцию для парсинга куки
+    //в данном случае, думаю, это допустимо, так как я сам создаю куки
+    let cookie = cParser(req.headers["cookie"]);
+    //
+    if(cookie !== " 0"){
+        let answer = cookie.split(" ")[0];
+        let cnt = cookie.split(" ")[1];
+        let prom = new Promise((resl, rej)=>{
+            let hrf = "";
+            let news = [];
+            if(isNaN(cnt)){
+                cnt = 9;
+            }
+            if(answer == "a1"){
+                hrf = "https://news.yandex.ru/business.html?from=rubric";
+            } else if(answer == "a2"){
+                hrf = "https://news.yandex.ru/society.html?from=rubric";
+            } else if(answer == "a3"){
+                hrf = "https://news.yandex.ru/politics.html?from=rubric";
+            }
+
+            request(hrf, (error, response, html) =>{
+                if(!error && response.statusCode == 200){
+                    let $ = cheerio.load(html);
+                    $(".story__title").children("a.link").each((i, elem)=>{
+                        if(i < cnt){
+                            news.push($(elem).text());
+                        }
+                    });
+                    resl(news);
+                }
+            });
+        });
+        prom.then((result)=>{
+            /*res.cookie("params", `answer${answer}*count${cnt}`);
+            console.log(cnt+ "at first render");*/
+            res.render('hello', {
+                news: result
+            });
+        }, (error)=>{
+            console.log(error);
+        })
+
+    } else {
+        res.render('hello', {
+            news: ['Выберите новости']
+        });
+    }
+});
+app.post('/news', function(req, res){
+    app.use(bodyParser.urlencoded({extended: false}));
+    let prom = new Promise((resl, rej)=>{
+        let hrf = "";
+        let news = [];
+        let cnt = 9;
+        if(!isNaN(req.body.count)){
+            cnt = req.body.count;
+        }
+        if(req.body.answer == "a1"){
+            hrf = "https://news.yandex.ru/business.html?from=rubric";
+        } else if(req.body.answer == "a2"){
+            hrf = "https://news.yandex.ru/society.html?from=rubric";
+        } else if(req.body.answer == "a3"){
+            hrf = "https://news.yandex.ru/politics.html?from=rubric";
+        }
+
+        request(hrf, (error, response, html) =>{
+            if(!error && response.statusCode == 200){
+                let $ = cheerio.load(html);
+                $(".story__title").children("a.link").each((i, elem)=>{
+                    if(i < cnt){
+                        news.push($(elem).text());
+                    }
+                });
+                resl(news);
+            }
+        });
+    });
+    prom.then((result)=>{
+        res.cookie("params", `answer${req.body.answer}*count${req.body.count}`);
+        res.render('hello', {
+            news: result
+        });
+    }, (error)=>{
+        console.log(error);
+    })
 });
 
-rl.question('Введите текст (RU):', (textIn) =>{
-    textTrans(textIn);
-    rl.close(); 
-    }
-);
 
-const textTrans = (textInput) => {
-   key = 'trnsl.1.1.20181106T083457Z.87ac33d9ca391bbb.610e70224699c3872400711cb6f4dc812ca6ebe1';
-   url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
-   lang = 'ru-en';
-
-  request(url, {form: {key: key, text: textInput, lang: lang}},
-  (err, res, body) => {
-    if(!err && res.statusCode === 200) {
-        console.log(chalk.white.bgRed (` Перевод(EN):${JSON.parse(body).text[0]} `));
-    } else {
-        console.log(chalk.white.bgRed(' Произошла ошибка при выполнении запроса '));
+let cParser = (data) =>{
+    let answer ="";
+    let count = 0;
+    if(data){
+        let result = data.split("; ");
+        result.forEach((line)=>{
+            if(!line.indexOf("params")){
+                answer = line.split("answer")[1].split("*")[0];
+                count = line.split("count")[1];
+            }
+        });
     }
-  });
+    return answer + " " + count;
 };
+app.listen(3000);
