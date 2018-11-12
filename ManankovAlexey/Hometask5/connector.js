83251017c58d9988fc260df2ff86da8f09b68b94
Alexey <virtbrain@gmail.com>
2018-11-12 20:42:05 +0500

    Comments delete

--- ManankovAlexey/Hometask5/connector.js
+++ ManankovAlexey/Hometask5/connector.js
@@ -1,7 +1,3 @@
 const mongoose = require(`mongoose`);
 
 mongoose.connect(`mongodb://localhost/todolist`)
-
-// const db = mongoose.connection;
-// db.on(`error`, console.error.bind(console, `connection error:`));
-// db.once('open', console.log(`Connection successful`));
