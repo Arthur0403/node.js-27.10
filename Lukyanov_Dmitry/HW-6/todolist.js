c2985a4a58dc0b3b887931a8060a68a653903eeb
Dmitry <lukasm@yandex.ru>
2018-11-16 22:07:01 +0300

    правка

--- Lukyanov_Dmitry/HW-6/todolist.js
+++ Lukyanov_Dmitry/HW-6/todolist.js
@@ -10,26 +10,33 @@ var todo = require('./models/todo');
 
 
 passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
+  function (username, password, cb) {
+    db.users.findByUsername(username, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      if (user.password != password) {
+        return cb(null, false);
+      }
       return cb(null, user);
     });
   }));
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser(function (id, cb) {
   db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
+    if (err) {
+      return cb(err);
+    }
     cb(null, user);
   });
 });
-
 
 var app = express();
 mongoose.connect('mongodb://localhost/todo');
@@ -37,105 +44,109 @@ mongoose.connect('mongodb://localhost/todo');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require('body-parser').urlencoded({
+  extended: true
+}));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(express.static(path.join(__dirname, 'models')));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 
 
-
-// Define routes.
-
 app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
+  function (req, res) {
+    res.render('home', {
+      user: req.user
+    });
   });
 
 
 app.get('/list', function (req, res) {
   todo.find({}, function (err, todoList) {
-      if (err){
-          console.log(err);
-      } else {
-          res.render('list', { allList: todoList });
-          require('connect-ensure-login').ensureLoggedIn(),
-          function(req, res){
-          res.render('profile', { user: req.user });
-          }
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('list', {
+        allList: todoList
+      });
+      require('connect-ensure-login').ensureLoggedIn(),
+        function (req, res) {
+          res.render('profile', {
+            user: req.user
+          });
         }
-       });
+    }
+  });
 });
 
 app.get('/login',
-  function(req, res){
+  function (req, res) {
     res.render('login');
   });
-  
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
+
+app.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
     res.redirect('/list');
-    
+
   });
 
-  app.get('/profile',
+app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
+  function (req, res) {
+    res.render('profile', {
+      user: req.user
+    });
   });
 
-
-  app.get('/logout',
-  function(req, res){
+app.get('/logout',
+  function (req, res) {
     req.logout();
     res.redirect('/');
   });
 
 
-
- 
-
-
 app.post('/add-todo', (req, res) => {
   const addTodo = new todo({
-  name: req.body.item,
+    name: req.body.item,
   });
   todo.create(addTodo, (err, todo) => {
-  if (err) console.log(err);
+    if (err) console.log(err);
   });
   res.redirect('/list');
 });
-
 
 app.post('/del-todo', (req, res) => {
   const id = req.body.id;
   todo.findByIdAndRemove(id, (err) => {
-  console.log(err);    
+    console.log(err);
   });
-   res.redirect('/list');
-});
-
-
-app.post('/edit-todo', (req, res) => {
-  const id = req.body.edit;
-   todo.findByIdAndUpdate(id, {new: true}, (err) => {
-      if (err) console.log(err);
-      });
   res.redirect('/list');
 });
 
-  
+app.post('/edit-todo', (req, res) => {
+  const id = req.body.edit;
+  todo.findByIdAndUpdate(id, {
+    new: true
+  }, (err) => {
+    if (err) console.log(err);
+  });
+  res.redirect('/list');
+});
 
 
-  app.listen(3000, function () {
-    console.log('Server started port 3000');
+app.listen(3000, function () {
+  console.log('Server started port 3000');
 });
