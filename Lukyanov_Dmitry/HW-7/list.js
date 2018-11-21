58fad0e29bd2026dd4a1da465c8156912988c876
Dmitry <lukasm@yandex.ru>
2018-11-21 13:36:55 +0300

    Кое что поправил

--- Lukyanov_Dmitry/HW-7/list.js
+++ Lukyanov_Dmitry/HW-7/list.js
@@ -21,42 +21,75 @@ app.use(express.static
 ));
 
 
+//user
+
+function identifyUser(req, res, next) {
+  if(req.headers.authorization) {
+    const [, token] = req.headers.authorization.split(' ');
+
+    jwt.verify(token, 'secret', function(err, decoded) {
+      if(err) {
+        return res.status(403).json({ error: 'Wrong token' });
+      }
+
+      req.user = decoded;
+      next();
+    });
+  } else {
+    res.status(403).json({ error: 'No token present' });
+  }
+}
+
+app.post('/auth', (req, res) => {
+  const { username, password } = req.body;
+
+  if(username === 'admin' && password === 'admin') {
+    const token = jwt.sign({
+      id: 1,
+      username: 'admin',
+      fullName: 'Vasya Pupkin',
+    }, 'secret');
+
+    res.json({ token });
+  } else {
+    res.json({ error: 'Wrong credentials' });
+  }
+});
+
+app.all('/todo', identifyUser);
+app.all('/todos*', identifyUser);
+
+
 // Todo //
 
 app.get('/todos', async (req, res) => {
   const todos = await Todo.find();
-
   res.json(todos);
 });
 
 app.get('/todos/:id', async (req, res) => {
   const todo = await Todo.findById(req.params.id);
-
   res.json(todo);
 });
 
 app.post('/todos', async (req, res) => {
   let todo = new Todo(req.body);
   todo = await todo.save();
-
   res.json(todo);
 });
 
 app.put('/todo/:id', async (req, res) => {
   const todo = await Todo.update({ _id: req.params.id }, req.body);
-
   res.json(todo);
 });
 
 app.patch('/todo/:id', async (req, res) => {
   const todo = await Todo.update({ _id: req.params.id }, { $set: req.body });
-
   res.json(todo);
 });
 
 app.delete('/todo/:id', async (req, res) => {
   const todo = await Todo.remove({ _id: req.params.id });
-
   res.json(todo);
 });
 
