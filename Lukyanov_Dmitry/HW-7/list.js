14e20f73acffcf332acbe3fb24355243e3485573
Dmitry <lukasm@yandex.ru>
2018-11-21 12:54:54 +0300

    Домашнее задание HW-7

--- Lukyanov_Dmitry/HW-7/list.js
+++ Lukyanov_Dmitry/HW-7/list.js
@@ -2,85 +2,64 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true });
-
 const jwt = require('jsonwebtoken');
+const path = require('path');
+
 const User = require('./models/user');
 const Todo = require('./models/todo');
 
+mongoose.connect('mongodb://localhost/todo');
 const app = express();
+
+
 
 app.use(bodyParser.json());
 app.use(cors());
 
-function identifyUser(req, res, next) {
-  if(req.headers.authorization) {
-    const [, token] = req.headers.authorization.split(' ');
-
-    jwt.verify(token, 'secret', function(err, decoded) {
-      if(err) {
-        return res.status(403).json({ error: 'Wrong token' });
-      }
-      req.user = decoded;
-      next();
-    });
-  } else {
-    res.status(403).json({ error: 'No token present' });
-  }
-}
+app.use(express.static
+  (path.join(__dirname, 'models')
+));
 
 
-app.post('/auth', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username, password })
-  if (user) {
-      const { _id: id, username, displayName } = user;
-      res.json({
-          access_token: jwt.sign({ id, username, displayName }, 'secret'),
-      })
-  } else {
-      res.json({ code: 1, message: 'Wrong credentials' });
-  }
+// Todo //
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
 });
 
-app.all('/users', identifyUser);
-app.all('/users*', identifyUser);
+app.get('/todos/:id', async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
 
-app.get('/users', async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+  res.json(todo);
 });
 
-app.get('/users/:id', async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.json(user);
+app.post('/todos', async (req, res) => {
+  let todo = new Todo(req.body);
+  todo = await todo.save();
+
+  res.json(todo);
 });
 
-app.post('/users', async (req, res) => {
-  let user = new User(req.body);
-  user = await user.save();
+app.put('/todo/:id', async (req, res) => {
+  const todo = await Todo.update({ _id: req.params.id }, req.body);
 
-  res.json(user);
+  res.json(todo);
 });
 
-app.put('/user/:id', async (req, res) => {
-  const user = await User.update({ _id: req.params.id }, req.body);
+app.patch('/todo/:id', async (req, res) => {
+  const todo = await Todo.update({ _id: req.params.id }, { $set: req.body });
 
-  res.json(user);
+  res.json(todo);
 });
 
-app.patch('/user/:id', async (req, res) => {
-  const user = await User.update({ _id: req.params.id }, { $set: req.body });
+app.delete('/todo/:id', async (req, res) => {
+  const todo = await Todo.remove({ _id: req.params.id });
 
-  res.json(user);
+  res.json(todo);
 });
 
-app.delete('/user/:id', async (req, res) => {
-  const user = await User.remove({ _id: req.params.id });
-
-  res.json(user);
-});
 
 app.listen(3000, function () {
   console.log('Server started port 3000');
--- Lukyanov_Dmitry/HW-7/models/todo.js
+++ Lukyanov_Dmitry/HW-7/models/todo.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TodoSchema = new mongoose.Schema({
+  id: Number,
   name: String,
-  done: Boolean,
+  completed: {type: Boolean, default: false},
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
