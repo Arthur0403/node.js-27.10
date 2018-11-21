6266b230cbc0eb1d9bdb741448fefcac27ba7bcf
dvbondarchuk@gmail.com <dvbondarchuk@gmail.com>
2018-11-21 22:01:10 +0300

    Lesson 8

---
+++ socket.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const Socket = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+const io = Socket(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+
+io.on('connection', (socket) => {
+  console.log('User has been connected');
+  
+  socket.on('message', (message) => {
+    message.timestamp = new Date();
+    socket.broadcast.emit('message', message);
+    socket.emit('message', message);
+  });
+});
+
+server.listen(8888, () => {
+  console.log('Server has been started on port: 8888');
+});
