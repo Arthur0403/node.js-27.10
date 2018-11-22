87a8bc38f1d7657df32c4b29dc9525751f573633
Dmitry <lukasm@yandex.ru>
2018-11-22 09:27:09 +0300

    Исправил ошибки

--- Lukyanov_Dmitry/HW-7/models/todo.js
+++ Lukyanov_Dmitry/HW-7/models/todo.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TodoSchema = new mongoose.Schema({
-  id: Number,
   name: String,
   completed: {type: Boolean, default: false},
 });
